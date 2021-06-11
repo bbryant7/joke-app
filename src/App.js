8de203570b5ab120a846a880664a0d5cd1a1d8b8
Bailey <bailey.bryant10@gmail.com>
2021-06-11 15:49:26 -0500

    add click event, update readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [answer, displayAnswer] = useState(false);
+  let answerText = answer ? <h2>He was a fungi!</h2> : ""
+  let button = answer ? "" : <button onClick={() => displayAnswer(true)}>Tell Me!</button>
   return (
     <div className="App">
       <h1> Why did the mushroom go to the party?</h1>
-      <button>Tell Me!</button>
+      {button}
+      {answerText}
     </div>
   );
 }
